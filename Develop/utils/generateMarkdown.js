4a1034a152f61b813659ec3357cd411b3567ebec
Muhammad Kamran <mkamran1967@gmail.com>
2020-09-16 02:05:46 -0700

    Markdown work

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -16,18 +16,25 @@ ${data.installation ? `## Installation \n ${data.installation}` : ''}
 
 ## Usage
 ${data.usage}
+${data.usage ? `## Installation \n ${data.usage}` : ''}
 
 ## License ${data.badge}
 ${data.license}
+${data.license ? `## Installation \n ${data.license}` : ''}
 
 ## Contributing
 ${data.contributing}
+${data.contributing ? `## Installation \n ${data.contributing}` : ''}
 
 ## Testing
 ${data.testing}
+${data.testing ? `## Installation \n ${data.testing}` : ''}
 
 ## Questions
-${da}
+${data.}
+${data.installation ? `## Installation \n ${data.installation}` : ''}
+
+
 `;
 }
 
